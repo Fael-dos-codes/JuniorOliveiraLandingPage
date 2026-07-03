28c9e444d602e46589cb3eb92168f94d7113d8dd
Fael dos Codes <rafaelfe0798@gmail.com>
2026-07-03 17:45:08 -0300

    fix: configurar rotas com react-router-dom e adicionar _redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WebsiteHome from './pages/WebsiteHome';
+import LandingPage from './pages/LandingPage';
 
 export default function App() {
-  return <WebsiteHome />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WebsiteHome />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
 }
